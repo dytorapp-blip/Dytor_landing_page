f2d9b93a3927058d142a2fe325ea753d05471b03
Nnadi Joshua <chimdijos8@gmail.com>
2025-11-23 22:15:19 +0100

    feat: Update particle resize configuration in SparklesCore component for improved responsiveness

--- components/ui/sparkles.tsx
+++ components/ui/sparkles.tsx
@@ -81,7 +81,7 @@ export const SparklesCore = (props: ParticlesProps) => {
                   enable: false,
                   mode: 'repulse',
                 },
-                resize: true,
+                resize: { enable: true },
               },
               modes: {
                 push: {
