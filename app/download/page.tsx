ba1e508947ad993c038b90a38fc806dd9a5d449f
Nnadi Joshua <chimdijos8@gmail.com>
2025-11-27 01:17:47 +0100

    fix: update download link for 64-bit EXE

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -49,7 +49,7 @@ export default function DownloadPage() {
 
 							<CardFooter>
 								<Button asChild size="lg" className="w-full" aria-label="Download DYTOR for Windows 64-bit">
-									<a href="/downloads/DytorSetup.exe">Download 64-bit EXE</a>
+									<a href="/DytorSetup.exe">Download 64-bit EXE</a>
 								</Button>
 							</CardFooter>
 						</Card>
