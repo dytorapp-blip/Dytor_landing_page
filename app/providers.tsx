45d4e7b79eaab2afa85070de5005eb767ba802c0
Nnadi Joshua <chimdijos8@gmail.com>
2025-12-11 13:06:48 +0100

    feat: introduce global providers for theming and authentication.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
 import * as React from "react";
 
 import { AuthProvider } from "@/components/contexts/session-provider";
